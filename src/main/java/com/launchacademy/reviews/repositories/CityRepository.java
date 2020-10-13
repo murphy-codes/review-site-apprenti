183d0e9cc5a458de32ae5807d010848a197445e1
John Bobst <johnabobst@gmail.com>
2020-10-13 11:44:19 -0700

    Added repositories

---
+++ src/main/java/com/launchacademy/reviews/repositories/CityRepository.java
@@ -0,0 +1,7 @@
+package com.launchacademy.reviews.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CityRepository extends PagingAndSortingRepository<City, Integer> {
+
+}
