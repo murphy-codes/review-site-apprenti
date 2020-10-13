80871adbbd49071cf0bad721f0efbcf93a0dd22c
John Bobst <johnabobst@gmail.com>
2020-10-13 14:53:32 -0700

    Added annotation to root controller

--- src/main/java/com/launchacademy/reviews/controllers/ReviewSiteController.java
+++ src/main/java/com/launchacademy/reviews/controllers/ReviewSiteController.java
@@ -1,11 +1,12 @@
 package com.launchacademy.reviews.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class ReviewSiteController {
   @GetMapping(value = "/**/{path:[^\\.]*}")
   public String forward() {
     return "forward:/";
   }
-
 }
