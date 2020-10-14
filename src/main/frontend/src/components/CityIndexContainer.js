5f910fd3a53a060dfcee719e58006f3c0f7ad61e
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-10-14 15:08:40 -0700

    Added pagination for the cities index page

--- src/main/frontend/src/components/CityIndexContainer.js
+++ src/main/frontend/src/components/CityIndexContainer.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react"
 import CityTile from "./CityTile"
 
-
 const CityIndexContainer = props => {
+  const [page, setPage] = useState(0)
+  const [size, setSize] = useState(5)
+  const [last, setLast] = useState(false)
+
   const [cities, setCities] = useState([])
   useEffect(() => {
-    fetch('/api/v1/cities')
+    fetch(`api/v1/cities?size=${size}&page=${page}`)
     .then(result => result.json())
     .then(cities => {
+      setLast(cities.last)
       setCities(cities.content)
     })
-  }, [])
+  }, [page,size])
+
   const cityTiles = cities.map(city => {
     return (
      <CityTile
@@ -22,9 +27,26 @@ const CityIndexContainer = props => {
      />
    );
  });
+
+ const prev = () => {
+   event.preventDefault();
+   setPage(page-1);
+ }
+
+ const next = () => {
+   event.preventDefault();
+   setPage(page+1);
+ }
+
+ let prevButton = page != 0 ? (<a href="#" onClick={prev}><i className="fas fa-angle-double-left soft-button"></i></a>) : null;
+ let nextButton = !last ? (<a href="#" onClick={next}><i className="fas fa-angle-double-right soft-button"></i></a>) : null;
+
  return (
-   cityTiles
-   
+   <>
+     <br/>
+     {cityTiles}
+     <p className="center-text">{prevButton} Pg: {page+1} {nextButton}</p>
+   </>
  )
 }
 
