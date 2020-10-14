4902ed90e56516843e881eb845c58faa9a11f8be
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-10-14 16:15:06 -0700

    Translated reviews into stars (rounded to the nearest half star)

--- src/main/frontend/src/components/CityShow.js
+++ src/main/frontend/src/components/CityShow.js
@@ -1,4 +1,5 @@
 import React from "react"
+import RatingStars from "./RatingStars"
 
 const CityShow = props => {
   return (
@@ -6,12 +7,13 @@ const CityShow = props => {
       <h2>{props.name}</h2>
       <img src={props.imageUrl} alt={props.name} className="city-img" />
       <p><br/>Description: {props.description}</p>
-      <p>
-        <span>Cost: {props.cost}</span><br/>
-        <span>Fun: {props.fun}</span><br/>
-        <span>Safety: {props.safety}</span>
-      </p>
+      <div className="margin-left-2 review-stars">
+        <span>Cost: </span><RatingStars rating={props.cost}/><br/>
+        <span>Fun: </span><RatingStars rating={props.fun}/><br/>
+        <span>Safety: </span><RatingStars rating={props.safety}/>
+      </div>
     </div>
   )
 }
 export default CityShow
+
---
+++ src/main/frontend/src/functions/roundHalf.js
@@ -0,0 +1,5 @@
+const roundHalf = (num) => {
+  return Math.round(num*2)/2;
+}
+
+export default roundHalf;
