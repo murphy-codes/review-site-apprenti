2abce16ace90ab764c7b13ded24b42819d12325b
John Bobst <johnabobst@gmail.com>
2020-10-14 09:26:01 -0700

    Worked on city index page
    Styled thumbnails, added fetch call

--- src/main/frontend/src/components/CityTile.js
+++ src/main/frontend/src/components/CityTile.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const CityTile = props => {
   return (
     <div className="city-tile row">
-      <Link to={`/cities/${props.id}`} className="city-thumbnail float-left margin-right-1 margin-left-2 margin-bottom-1"><img src={props.thumbnail} alt={`${props.name}`} /></Link>
+      <Link to={`/cities/${props.id}`} className="city-thumbnail float-left margin-right-1 margin-left-2 margin-bottom-1"><img className="city-image" src={props.thumbnail} alt={`${props.name}`} /></Link>
       <div>
         <h2 className="center-text"><Link to={`/cities/${props.id}`}> {props.name} </Link></h2>
         <p className="center-text">
