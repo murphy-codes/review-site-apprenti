8f0e6308cd0d34752314e2fbdbb22785c5a32021
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-10-22 09:31:41 -0700

    Added a select for page sizing on the cities index

--- src/main/frontend/src/components/CityIndexContainer.js
+++ src/main/frontend/src/components/CityIndexContainer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 import CityTile from "./CityTile"
 
 const CityIndexContainer = props => {
@@ -17,38 +18,75 @@ const CityIndexContainer = props => {
     })
   }, [page,size,props.match.params.searchTerm])
 
-  const cityTiles = cities.map(city => {
-    return (
-     <CityTile
-      key={city.id}
-      id={city.id}
-      name={city.name}
-      description={city.description}
-      thumbnail={city.imageUrl}
-     />
-   );
- });
+  let showPaging = true;
+  let cityTiles;
+  if (cities.length > 0) {
+    cityTiles = cities.map(city => {
+      return (
+        <CityTile
+          key={city.id}
+          id={city.id}
+          name={city.name}
+          description={city.description}
+          thumbnail={city.imageUrl}
+        />
+      );
+    });
+  } else if (location.pathname == "/") {
+    cityTiles = (<h2 className="loading-fetch center-text">Loading</h2>);
+  } else {
+    showPaging = false;
+    cityTiles = (
+    <div className="no-results center-text">
+      <h2>No results found!</h2>
+      <p className="center-text"><Link to="/">Return home?</Link></p>
+    </div>
+    );
+  }
 
- const prev = () => {
+  const handleSizeChange = (event) => {
+   setSize(event.target.value);
+   setPage(0);
+  }
+
+  const prev = () => {
    event.preventDefault();
    setPage(page-1);
- }
+  }
 
- const next = () => {
+  const next = () => {
    event.preventDefault();
    setPage(page+1);
- }
+  }
 
- let prevButton = page != 0 ? (<a href="#" onClick={prev}><i className="fas fa-angle-double-left soft-button"></i></a>) : null;
- let nextButton = !last ? (<a href="#" onClick={next}><i className="fas fa-angle-double-right soft-button"></i></a>) : null;
+  let prevButton = page != 0 ? (<a href="#" onClick={prev}><i className="fas fa-angle-double-left soft-button"></i></a>) : null;
+  let nextButton = !last ? (<a href="#" onClick={next}><i className="fas fa-angle-double-right soft-button"></i></a>) : null;
 
- return (
-   <>
-     <br/>
-     {cityTiles}
-     <p className="center-text">{prevButton} Pg: {page+1} {nextButton}</p>
-   </>
- )
+  let paging;
+  if (showPaging) {
+    paging = (
+      <>
+        <div className="select-size-container auto-margin">
+          <label htmlFor="size" className="inline">Results per page:</label>
+          <select name="size" id="size" className="select-size inline" onChange={handleSizeChange}>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+          </select>
+        </div>
+        <p className="center-text">{prevButton} Pg: {page+1} {nextButton}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <br/>
+      {cityTiles}
+      {paging}
+    </>
+  )
 }
 
 export default CityIndexContainer
